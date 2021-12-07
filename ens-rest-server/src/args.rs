780a6f5b40295a3e225202dc62c874f56dce9429
EnormousCloud <enormous@webcerebrium.com>
2021-12-07 09:41:39 +0200

    server: CLI args

---
+++ ens-rest-server/src/args.rs
@@ -0,0 +1,26 @@
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt, Clone)]
+#[structopt(name = "ens-rest-server", about = "ENS REST API Server")]
+pub struct Args {
+    /// Net listening address of HTTP server
+    #[structopt(long, default_value = "0.0.0.0:8000", env = "LISTEN")]
+    pub listen: String,
+    /// Ethereum JSON+RPC HTTP address
+    #[structopt(long, default_value = "http://localhost:8545", env = "RPC_ENDPOINT")]
+    pub rpc_endpoint: String,
+    /// ENS contract address
+    #[structopt(long, default_value = "", env = "ENS_CONTRACT")]
+    pub ens_contract: String,
+}
+
+pub fn parse() -> anyhow::Result<Args> {
+    dotenv::dotenv().ok();
+    let log_level: String = std::env::var("LOG_LEVEL").unwrap_or("info".to_owned());
+    tracing_subscriber::fmt()
+        .with_env_filter(tracing_subscriber::EnvFilter::new(log_level))
+        .init();
+    let res = Args::from_args();
+    tracing::debug!("{:?}", res);
+    Ok(res)
+}
--- ens-rest-server/src/main.rs
+++ ens-rest-server/src/main.rs
@@ -2,6 +2,12 @@ pub mod api;
 pub mod args;
 pub mod web3sync;
 
-pub fn main() {
-    println!("ens-rest-server")
+pub fn main() -> anyhow::Result<()> {
+    let args = match args::parse() {
+        Ok(x) => x,
+        Err(e) => return Err(anyhow::Error::msg(format!("Args parsing error {}", e))),
+    };
+
+    println!("ens-rest-server");
+    Ok(())
 }
